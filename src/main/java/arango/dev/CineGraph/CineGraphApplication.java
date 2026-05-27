df9d79b378a16e63653234c81944bd21c974ae09
CCodeKey <newcodiguin@gmail.com>
2026-05-27 19:05:01 -0300

    first commit

---
+++ src/main/java/arango/dev/CineGraph/CineGraphApplication.java
@@ -0,0 +1,13 @@
+package arango.dev.CineGraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arango/dev/CineGraph/CineGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package arango.dev.CineGraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
